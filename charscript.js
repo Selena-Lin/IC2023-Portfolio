4bcffd7ef0b7ee2957fca5218cfaf88a99396672
Selena-Lin <slin060606@gmail.com>
2023-06-30 14:37:44 -0400

    overlay
    changed overlay & positioned arrows

--- charscript.js
+++ charscript.js
@@ -1,9 +1,16 @@
+let openButton = document.querySelector(".open")
+let closeButton = document.querySelector(".close");
+
 /* Open when someone clicks on the span element */
-function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-  }
+openButton.onclick=function(){
+    document.getElementById("myNav").style.width = "80%";
+    openButton.style.display="none";
+    closeButton.style.display= "block";
+}
   
   /* Close when someone clicks on the "x" symbol inside the overlay */
-  function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-  }
+closeButton.onclick = function(){
+    document.getElementById("myNav").style.width = "20%";
+    openButton.style.display="block";
+    closeButton.style.display= "none";
+}
