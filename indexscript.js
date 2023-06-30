3b06a0a3fced632be6b3d0187840bc9d0e6cb026
Selena-Lin <slin060606@gmail.com>
2023-06-30 13:45:19 -0400

    js update
    Made js animations for index. Button changes brightness of dull image and redirects after 3 seconds

--- indexscript.js
+++ indexscript.js
@@ -1,1 +1,17 @@
 //script for index
+let enter = document.querySelector("button");
+let dullimage = document.querySelector(".dullpfp");
+
+enter.onclick=function(){
+    console.log("test")
+    dullimage.style.opacity= "1"
+    dullimage.style.boxShadow= "0 0 30px goldenrod"
+    dullimage.style.borderStyle = "groove"
+    dullimage.style.borderWidth = "3px"
+    dullimage.style.borderColor = "goldenrod"
+    setTimeout(callBack_func, 3000);
+
+}
+function callBack_func() {
+    window.location.href = "character.html";
+}
