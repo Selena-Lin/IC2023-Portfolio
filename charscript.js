1651c27e59c83ff7f3bbbc4f372a74cfa1eeb2a2
Selena-Lin <slin060606@gmail.com>
2023-07-04 22:07:01 -0400

    Link profile page
    Put in images to profile/bio and linked them all, made center div for content (blank) started on project list

--- charscript.js
+++ charscript.js
@@ -1,4 +1,4 @@
-let openButton = document.querySelector(".open")
+let openButton = document.querySelector(".open");
 let closeButton = document.querySelector(".close");
 
 /* Open when someone clicks on the span element */
@@ -14,3 +14,47 @@ closeButton.onclick = function(){
     openButton.style.display="block";
     closeButton.style.display= "none";
 }
+
+// variables for menu and img on right side
+let charSelect = document.querySelector(".me");
+let charList = document.querySelector(".charicons");
+
+// take back to characters menu
+let charButton = document.querySelector(".char");
+
+charButton.onclick = function(){
+  charList.style.display="flex";
+  bioMenu.style.display="none";
+}
+
+//specific to huowu
+let bio = document.querySelector(".profile");
+let bioMenu = document.querySelector(".huowu");
+let basicImg = document.querySelector(".basic");
+let extrac = document.querySelector(".extra");
+let why = document.querySelector(".code");
+
+bio.onclick = function(){
+  charList.style.display= "none";
+  bioMenu.style.display= "flex";
+  basicImg.style.opacity= "30%";
+  extrac.style.opacity= "100%";
+  why.style.opacity= "100%";
+}
+basicImg.onclick = function(){
+  basicImg.style.opacity= "30%";
+  extrac.style.opacity= "100%";
+  why.style.opacity= "100%";
+}
+extrac.onclick = function(){
+  basicImg.style.opacity= "100%";
+  extrac.style.opacity= "30%";
+  why.style.opacity= "100%";
+}
+why.onclick = function(){
+  basicImg.style.opacity= "100%";
+  extrac.style.opacity= "100%";
+  why.style.opacity= "30%";
+}
+
+//diaochan
